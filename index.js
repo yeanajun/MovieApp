8b52c2bc1de1fa7370c26d7b4487f09241d09173
yeanajun <qkqlalsk@naver.com>
2021-01-18 01:30:12 +0900

    처음 저장소에 올림

---
+++ index.js
@@ -0,0 +1,13 @@
+const express = require('express')
+const app = express()
+const port = 5000
+
+const mongoose = require('mongoose')
+mongoose.connect('mongodb+srv://yeanajun:yeanajun220@boilerplate.kgolb.mongodb.net/<dbname>?retryWrites=true&w=majority', {
+    useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true, useFindAndModify:false
+}).then(()=> console.log('MongoDB Connected...'))
+  .catch(err => console.log(err))
+
+app.get('/', (req, res) => res.send('Hello world!'))
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`))
