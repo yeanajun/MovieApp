11a9845c2949f1fb59deb4d90f20fee6f44974c6
yeanajun <qkqlalsk@naver.com>
2021-01-19 15:31:39 +0900

    비밀 정보 보호

---
+++ config/prod.js
@@ -0,0 +1,3 @@
+module.exports = {
+    mongo_URI: process.env.MONGO_URI
+}
--- index.js
+++ index.js
@@ -2,12 +2,37 @@ const express = require('express')
 const app = express()
 const port = 5000
 
+const config = require('./config/key');
+
+const { User } = require("./models/User");
+const bodyParser = require('body-parser');
+
+
+app.use(bodyParser.urlencoded({extended: true}));
+
+app.use(bodyParser.json());
+
 const mongoose = require('mongoose')
-mongoose.connect('mongodb+srv://yeanajun:yeanajun220@boilerplate.kgolb.mongodb.net/<dbname>?retryWrites=true&w=majority', {
+mongoose.connect(config.mongoURI, {
     useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true, useFindAndModify:false
 }).then(()=> console.log('MongoDB Connected...'))
   .catch(err => console.log(err))
 
+
 app.get('/', (req, res) => res.send('Hello world!'))
+
+
+app.post('/register', (req, res) => {
+  
+  const user = new User(req.body)
+
+
+  user.save((err, userInfo) => {
+    if (err) return res.json({success: false, err})
+    return res.status(200).json({
+      success: true
+    })
+  })
+})
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
