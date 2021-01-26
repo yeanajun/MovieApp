a9e000215e2e77366904372f723db1a4d009a493
yeanajun <qkqlalsk@naver.com>
2021-01-26 15:35:12 +0900

    boiler-plate 막

---
+++ client/src/hoc/auth.js
@@ -0,0 +1,45 @@
+import axios from 'axios';
+import React, {useEffect} from 'react';
+import{useDispatch} from 'react-redux';
+import {auth} from '../actions/user_action';
+
+
+export default function(SpecificComponent, option, adminRoute = null) {
+
+    function AuthenticationCheck(props) {
+
+        const dispatch = useDispatch();
+
+        useEffect(() => {
+
+            dispatch(auth()).then(response => {
+                console.log(response)
+
+
+                if(!response.payload.isAuth) {
+                    if(option) {
+                        props.history.push('/login')
+                    }
+                }else{
+                    if(adminRoute && !response.payload.isAdmin) {
+                        props.history.push('/')
+                    } else {
+                        if(option === false)
+                        props.history.push('/')
+                    }
+
+
+
+                }
+            })
+            
+        }, [])
+
+        return (
+            <SpecificComponent />
+        )
+    }
+
+
+    return AuthenticationCheck
+}
