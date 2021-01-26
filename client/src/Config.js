e1e60dbf22dcfaae86c2882677cd358fbfcafc07
yeanajun <qkqlalsk@naver.com>
2021-01-26 19:25:35 +0900

    movieapp

---
+++ client/src/Config.js
@@ -0,0 +1,5 @@
+export const USER_SERVER = '/api/users';
+
+export const API_KEY = 'a4bddda8b634a6f337ddcf203bfd263a';
+export const API_URL = 'https://api.themoviedb.org/3/';
+export const IMAGE_BASE_URL = 'https://image.tmdb.org/t/p/';
--- client/src/components/views/LandingPage/LandingPage.js
+++ client/src/components/views/LandingPage/LandingPage.js
@@ -1,42 +1,87 @@
-import React,{ useEffect } from 'react'
-import axios from 'axios'
-import {withRouter} from 'react-router-dom'
+import React, { useEffect, useState } from 'react'
+import { API_URL, API_KEY, IMAGE_BASE_URL } from '../../../Config'
+import MainImage from './Sections/MainImage'
+import GridCards from '../../views/commons/GridCards'
+import {Row} from 'antd'
 
 
-function LandingPage(props) {
+function LandingPage() {
+
+
+    const [Movies, setMovies] = useState([])
+    const [MainMovieImage, setMainMovieImage] = useState(null)
+    const [CurrentPage, setCurrentPage] = useState(0)
+
 
     useEffect(() => {
-        axios.get('/api/hello')
-        .then(response => {console.log(response.data)})
+        const endpoint = `${API_URL}movie/popular?api_key=${API_KEY}&language=en-US&page=1`;
+
+        fetchMovies(endpoint)
+
+
     }, [])
 
 
-    const onClickHandler = () => {
-        axios.get(`/api/users/logout`)
-        .then(response => {
-            if(response.data.success){
-                props.history.push("/login")
-            }else {
-                alert("로그아웃 하는데 실패했습니다.")
-            }
-
-        })
+    const fetchMovies = (endpoint) => {
+        fetch(endpoint)
+            .then(response => response.json())
+            .then(response => {
+                setMovies([...Movies,  ...response.results])
+                setMainMovieImage(response.results[0])
+                setCurrentPage(response.page)
+            })
     }
-    
+
+    const loadMoreItems = () => {
+        const endpoint = `${API_URL}movie/popular?api_key=${API_KEY}&language=en-US&page=${CurrentPage +1}`;
+
+        fetchMovies(endpoint)
+    }
+
 
     return (
-        <div style = {{
-            display: 'flex', justifyContent: 'center', alignItems: 'center'
-            , width: '100%', height: '100vh'
-        }}>
-            <h2>시작 페이지</h2>
+        <div style={{ width: '100%', margin: '0' }}>
+            {MainMovieImage &&
+                <MainImage
+                    image={`${IMAGE_BASE_URL}w1280${MainMovieImage.backdrop_path}`}
+                    title={MainMovieImage.original_title}
+                    text={MainMovieImage.overview} />
+            }
 
-            <button onClick = {onClickHandler}>
-                로그아웃
-            </button>
+            <div style={{ width: '85%', margin: '1rem auto' }}>
+                <h2>Movies by latest</h2>
+                <hr />
+
+                <Row gutter={[16,16]}>
+                    {Movies && Movies.map((movie, index ) => (
+                        <React.Fragment key = {index}>
+
+                            <GridCards 
+                                image = {movie.poster_path ?
+                                `${IMAGE_BASE_URL}w500${movie.poster_path}`: null}
+
+                                movieId={movie.id}
+                                movieName={movie.original_title}
+                            />
+                        </React.Fragment>
+                    ))}
+
+
+
+                </Row>
+
+
+            </div>
+
+            <div style={{ display: 'flex', justifyContent: 'center' }}>
+                <button onClick={loadMoreItems}>Load More</button>
+            </div>
+
+
 
         </div>
     )
 }
 
-export default withRouter(LandingPage)
+export default LandingPage
+
